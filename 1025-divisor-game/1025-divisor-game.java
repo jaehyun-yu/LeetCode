220e09e406f8bb4d7ab20f471601c7577ab36348
jaehyun-yu <80299111+jaehyun-yu@users.noreply.github.com>
2022-10-16 19:34:13 +0900

    Time: 0 ms (100.00%), Space: 40.5 MB (70.04%) - LeetHub

---
+++ 1025-divisor-game/1025-divisor-game.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean divisorGame(int n) {
+        boolean answer = false;
+        
+        if (n % 2 == 0) {
+            answer = true;
+        }
+        
+        return answer;
+    }
+}
